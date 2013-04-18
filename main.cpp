2063cae268eec84e692a917f550b65f623fbeddc
Yousef Maknie <ymaknie@localhost.localdomain>
2013-04-18 11:39:38 +0300

    18.04.2013 11:39

--- main.cpp
+++ main.cpp
@@ -2,11 +2,50 @@
 
 int main() {
 
-	Data_Transmission dt("pingu.ha.ax", "pop2013", "m1cr0Trans8", "pop2013");
+	std::string host = "";
+	std::string user = "";
+	std::string pw = "";
+	std::string db = "";
 
-	dt.connectToDB();
-	dt.storeQuery("SELECT * FROM gps_information;");
-	dt.sendQuery();
+	int i;
 
-	dt.writeData();
+	std::cout << "1. use default" << std::endl;
+	std::cout << "2. enter your own connection details" << std::endl;
+
+	std::cin >> i;
+
+	if(i == 1) {
+		host = "pingu.ha.ax";
+		user = "pop2013";
+		pw = "m1cr0Trans8";
+		db = "pop2013";
+	}
+	else {
+		std::cout << "Enter host IP" << std::endl;
+		std::cout << ">> ";
+		std::cin >> host;
+		std::cout << "Enter username" << std::endl;
+		std::cout << ">> ";
+		std::cin >> user;
+		std::cout << "Enter password" << std::endl;
+		std::cout << ">> ";
+		std::cin >> pw;
+		std::cout << "Enter database to connect to"<< std::endl;
+		std::cout << ">> ";
+		std::cin >> db;
+	}
+
+	if(host.size() == 0 || user.size() == 0 || pw.size() == 0 || db.size() == 0) {
+		std::cout << "no connection details entered" << std::endl;
+	} else {
+		Data_Transmission dt(host, user, pw, db);
+		
+		dt.connectToDB();
+		dt.storeQuery("SELECT * FROM gps_information;");
+		dt.sendQuery();
+		
+		dt.writeData();
+	}
+	
+	
 }
